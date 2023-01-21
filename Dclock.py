844bff04b3a9f0c0e548dc123dc5a774af614889
sagorrkr <ridoykum9@gmail.com>
2023-01-21 08:37:59 +0600

    main.py

---
+++ Dclock.py
@@ -0,0 +1,15 @@
+from tkinter import *
+from tkinter.ttk import *
+from time import strftime
+
+root = Tk()
+
+root.title("Digital clock")
+def clock():
+    tick = strftime("%H:%M:%S %p")
+    label.config(text =tick)
+    label.after(1000, clock)  
+label = Label(root, font = ("segoe", 80), foreground = "white", background = "black")
+label.pack(anchor= "center")
+clock()
+mainloop()
